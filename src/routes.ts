dec8dc595bb85c4d587fdacb732449da80782d7e
dvs2000 <dorivaldodossantos2000@gmail.com>
2021-01-21 04:12:30 +0100

    Save

--- src/routes.ts
+++ src/routes.ts
@@ -23,72 +23,72 @@ const routes = Router()
 
 // Rotas de autentição
 routes
-  .post('/auth', authController.login)
-  .post('/auth/admin', authController.loginAdmin)
-  .post('/auth/drive', authController.loginDrive)
-  .post('/forgetPassword', authController.forgotPassword)
-  .post('/resetPassword', authController.resetPassword)
-  .post('/ativeAccount', authController.ativeAccount)
-  .get('/ativeAccount/:id([0-9]+)/:code', authController.ativeAccount)
+  .post('/v2/auth', authController.login)
+  .post('/v2/auth/admin', authController.loginAdmin)
+  .post('/v2/auth/drive', authController.loginDrive)
+  .post('/v2/forgetPassword', authController.forgotPassword)
+  .post('/v2/resetPassword', authController.resetPassword)
+  .post('/v2/ativeAccount', authController.ativeAccount)
+  .get('/v2/ativeAccount/:id([0-9]+)/:code', authController.ativeAccount)
 
 // Rotas para operações do user
 routes
-  .post('/user', userController.create)
-  .delete('/user/:id([0-9]+)', userController.delete)
-  .get('/user/:id([0-9]+)', userController.getByIDorToken)
+  .post('/v2/user', userController.create)
+  .delete('/v2/user/:id([0-9]+)', userController.delete)
+  .get('/v2/user/:id([0-9]+)', userController.getByIDorToken)
 
 // JWT
 routes.use(checkJwt)
 routes
-  .get('/users', checkRole(4), userController.index)
-  .get('/user', userController.getByIDorToken)
-  .put('/user', userController.update)
-  .put('/user/:id([0-9]+)', userController.update)
-  .delete('/user', userController.delete)
-  .delete('/user/:id([0-9]+)', userController.delete)
+  .get('/v2/users', checkRole(1), userController.index)
+  .get('/v2/user', userController.getByIDorToken)
+  .put('/v2/user', userController.update)
+  .put('/v2/user/:id([0-9]+)', userController.update)
+  .delete('/v2/user', userController.delete)
+  .delete('/v2/user/:id([0-9]+)', userController.delete)
 
 // Rotas das operações do Tipo de Usuário
 routes
-  .get('/typeUser', checkRole(4), typeUserController.index)
-  .post('/typeUser', checkRole(4), typeUserController.create)
-  .put('/typeuser/:id([0-9]+)', checkRole(4), typeUserController.update)
-  .delete('/typeuser/:id([0-9]+)', checkRole(4), typeUserController.delete)
+  .get('/v2/typeUser', checkRole(1), typeUserController.index)
+  .post('/v2/typeUser', checkRole(1), typeUserController.create)
+  .put('/v2/typeuser/:id([0-9]+)', checkRole(1), typeUserController.update)
+  .delete('/v2/typeuser/:id([0-9]+)', checkRole(1), typeUserController.delete)
 
 // Rotas das operações da marca
 routes
-  .post('/marca', checkRole(4), marcaController.create)
-  .put('/marca/:id([0-9]+)', checkRole(4), marcaController.update)
-  .get('/marca', checkRole(4), marcaController.index)
-  .delete('/marca/:id([0-9]+)', checkRole(4), marcaController.delete)
+  .post('/v2/marca', checkRole(1), marcaController.create)
+  .put('/v2/marca/:id([0-9]+)', checkRole(1), marcaController.update)
+  .get('/v2/marca', checkRole(1), marcaController.index)
+  .delete('/v2/marca/:id([0-9]+)', checkRole(1), marcaController.delete)
 
 // Rotas para operações do Modelo
 routes
-  .get('/modelo', checkRole(4), modeloController.index)
-  .post('/modelo', checkRole(4), modeloController.create)
-  .put('/modelo/:id([0-9]+)', checkRole(4), modeloController.update)
-  .delete('/modelo/:id([0-9]+)', checkRole(4), modeloController.delete)
+  .get('/v2/modelo', checkRole(1), modeloController.index)
+  .post('/v2/modelo', checkRole(1), modeloController.create)
+  .put('/v2/modelo/:id([0-9]+)', checkRole(1), modeloController.update)
+  .delete('/v2/modelo/:id([0-9]+)', checkRole(1), modeloController.delete)
 
 // Rotas para operações do veiculos
 routes
-  .get('/veiculo', checkRole(4), veiculoController.index)
-  .post('/veiculo', checkRole(4), veiculoController.create)
-  .put('/veiculo/:id([0-9]+)', checkRole(4), veiculoController.update)
-  .delete('/veiculo/:id([0-9]+)', checkRole(4), veiculoController.delete)
-  .get('/veiculo/:matricula', checkRole(4), veiculoController.getByMatricula)
+  .get('/v2/veiculo', checkRole(1), veiculoController.index)
+  .post('/v2/veiculo', checkRole(1), veiculoController.create)
+  .put('/v2/veiculo/:id([0-9]+)', checkRole(1), veiculoController.update)
+  .delete('/v2/veiculo/:id([0-9]+)', checkRole(1), veiculoController.delete)
+  .get('/v2/veiculo/:matricula', checkRole(1), veiculoController.getByMatricula)
 
 routes
-  .get('/pedido', pedidoController.index)
-  .get('/pedido/:id([0-9]+)', pedidoController.getByID)
-  .get('/pedido/cancel/:id([0-9]+)', pedidoController.cancel)
-  .get('/pedido/countFinished', pedidoController.countFinished)
-  .get('/pedido/countCancel', pedidoController.countCancel)
-  .post('/pedido', pedidoController.store)
-  .put('/pedido/:id([0-9]+)', pedidoController.update)
-  .put('/pedido/estado/:id([0-9]+)', pedidoController.updateEstado)
-  .delete('/pedido/:id([0-9]+)', pedidoController.delete)
+  .get('/v2/pedido', pedidoController.index)
+  .get('/v2/pedido/:id([0-9]+)', pedidoController.getByID)
+  .get('/v2/pedido/cancel/:id([0-9]+)', pedidoController.cancel)
+  .get('/v2/pedido/countFinished', pedidoController.countFinished)
+  .get('/v2/pedido/countCancel', pedidoController.countCancel)
+  .post('/v2/pedido', pedidoController.store)
+  .put('/v2/pedido/:id([0-9]+)', pedidoController.update)
+  .put('/v2/pedido/estado/:id([0-9]+)', pedidoController.updateEstado)
+  .delete('/v2/pedido/:id([0-9]+)', pedidoController.delete)
 
 // Rota para fazer o uload dos ficheiros (imgs, pdf, video....)
-routes.post('/uploadFile', multerConfig.single('file'), async (req, res) => {
+routes.post('/v2/uploadFile', multerConfig.single('file'), async (req, res) => {
   const outputImage = `src/upload/${new Date().getTime().toString()}.jpg`
 
   sharp(req.file.path).resize({ height: 200, width: 200 }).toFile(outputImage).then(file => {
